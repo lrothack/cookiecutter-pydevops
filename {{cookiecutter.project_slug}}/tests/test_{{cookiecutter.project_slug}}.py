554b699605af7407624a3ad990aed817372e03ce
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-06-25 00:25:46 +0200

    Switching from unittest to pytest

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,31 +1,33 @@
-import unittest
+import pytest
 from argparse import Namespace
 import {{cookiecutter.project_slug}}.main
 
 
-class TestMain(unittest.TestCase):
+@pytest.fixture
+def args_ns_exp():
+    return Namespace()
 
-    def setUp(self):
-        self.__args_ns_exp = Namespace()
 
-    def test_parseargs(self):
-        arg_list = []
-        args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-        # Define expected result
-        self.__args_ns_exp.verbose = False
-        self.__args_ns_exp.quiet = False
-        self.assertEqual(args_ns, self.__args_ns_exp)
+def test_parseargs(args_ns_exp):
+    arg_list = []
+    args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
+    # Define expected result
+    args_ns_exp.verbose = False
+    args_ns_exp.quiet = False
+    assert args_ns == args_ns_exp
 
-    def test_parseargs_verbose(self):
-        arg_list = ['--verbose']
-        args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-        self.__args_ns_exp.verbose = True
-        self.__args_ns_exp.quiet = False
-        self.assertEqual(args_ns, self.__args_ns_exp)
 
-    def test_parseargs_quiet(self):
-        arg_list = ['--quiet']
-        args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-        self.__args_ns_exp.verbose = False
-        self.__args_ns_exp.quiet = True
-        self.assertEqual(args_ns, self.__args_ns_exp)
+def test_parseargs_verbose(args_ns_exp):
+    arg_list = ['--verbose']
+    args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
+    args_ns_exp.verbose = True
+    args_ns_exp.quiet = False
+    assert args_ns == args_ns_exp
+
+
+def test_parseargs_quiet(args_ns_exp):
+    arg_list = ['--quiet']
+    args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
+    args_ns_exp.verbose = False
+    args_ns_exp.quiet = True
+    assert args_ns == args_ns_exp
