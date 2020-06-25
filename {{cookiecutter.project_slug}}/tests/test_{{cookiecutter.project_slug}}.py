258540409ab7761d16d48e03e01be9f29c4396aa
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-06-25 11:58:01 +0200

    Switching from unittest to pytest

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -4,30 +4,34 @@ import {{cookiecutter.project_slug}}.main
 
 
 @pytest.fixture
-def args_ns_exp():
+def args_ns_ref():
     return Namespace()
 
 
-def test_parseargs(args_ns_exp):
+def test_parseargs(args_ns_ref):
     arg_list = []
     args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-    # Define expected result
-    args_ns_exp.verbose = False
-    args_ns_exp.quiet = False
-    assert args_ns == args_ns_exp
+    # Define reference/expected result
+    args_ns_ref.verbose = False
+    args_ns_ref.quiet = False
+    assert args_ns == args_ns_ref
 
 
-def test_parseargs_verbose(args_ns_exp):
+def test_parseargs_verbose(args_ns_ref):
     arg_list = ['--verbose']
     args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-    args_ns_exp.verbose = True
-    args_ns_exp.quiet = False
-    assert args_ns == args_ns_exp
+    args_ns_ref.verbose = True
+    args_ns_ref.quiet = False
+    assert args_ns == args_ns_ref
 
 
-def test_parseargs_quiet(args_ns_exp):
+def test_parseargs_quiet(args_ns_ref):
     arg_list = ['--quiet']
     args_ns = {{cookiecutter.project_slug}}.main.parse_args(arg_list)
-    args_ns_exp.verbose = False
-    args_ns_exp.quiet = True
-    assert args_ns == args_ns_exp
+    args_ns_ref.verbose = False
+    args_ns_ref.quiet = True
+    assert args_ns == args_ns_ref
+
+
+if __name__ == "__main__":
+    pytest.main()
